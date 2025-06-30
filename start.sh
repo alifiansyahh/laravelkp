2b4f93480c013db0481d23fdf1cfbd8bd4514f16
Alifffiansyah2 <mr4l3f123@gmail.com>
2025-07-01 01:51:06 +0700

    railway

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# Jalankan migrasi (opsional)
-php artisan migrate --force
+# Set permission
+chmod -R 775 storage bootstrap/cache
 
-# Jalankan Laravel server di host publik
+# Buat storage link (jika kamu pakai upload/cetak PDF)
+php artisan storage:link || true
+
+# Generate key (optional kalau belum di Variables)
+php artisan key:generate --force
+
+# Serve Laravel ke Railway
 php artisan serve --host=0.0.0.0 --port=8000
