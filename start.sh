26d7c07b6dd9d5724bb627d7fbb250ab76fb72b7
Alifffiansyah2 <mr4l3f123@gmail.com>
2025-07-01 23:09:50 +0700

    Add start.sh to fix Railway deployment

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Jalankan migrate
-php artisan migrate --force
-
-# Jalankan queue (opsional)
-# php artisan queue:work &
-
-# Jalankan Laravel dengan PHP built-in server
+# Jalankan Laravel dengan host dan port untuk Railway
 php artisan serve --host=0.0.0.0 --port=8080
